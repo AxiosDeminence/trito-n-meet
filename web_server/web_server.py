c35a5056a7bb338c7cde1cba6c3860ec5c3cb5df
Axios_Deminence <juhmer@att.net>
2019-11-28 23:04:47 -0800

    Re-consolidated code with refactored static methods

--- web_server/web_server.py
+++ web_server/web_server.py
@@ -1,7 +1,10 @@
+from contextlib import closing
+import re
 import datetime
 
 import falcon
 import psycopg2
+import argon2
 
 DATABASE = "d814roat3puk53"
 USER = "evsifgooyevaft"
@@ -11,11 +14,217 @@ PORT = "5432"
 
 CREDENTIALS = ("dbname=%s user=%s password=%s host=%s port=%s"
                % (DATABASE, USER, PASSWORD, HOST, PORT))
+               
+class CreateUser:
+    @staticmethod
+    def form_parser(form):
+        is_valid = True
+        try:
+            email = str.strip(form.get("email"))
+            full_name = str.strip(form.get("fullName"))
+            password = str.strip(form.get("password"))
+            confirm_password = str.strip(form.get("confirmPassword"))
+        except (KeyError, TypeError):
+            is_valid = False
+            result = "JSON Format Error"
+        else:
+            result = {"email": email, "full_name": full_name,
+                      "password": password,
+                      "confirm_password": confirm_password}
+        return {"is_valid": is_valid, "result": result}
 
-from CreateUser import CreateUser
-from GetFullName import GetFullName
-from UserLogin import UserLogin
+    @staticmethod
+    def validate(email, password, confirm_password):
+        is_valid = False
 
+        if password != confirm_password:
+            result = "Passwords are not the same"
+        elif not (re.search(r"[A-Z]", password) and
+                  re.search(r"[a-z]", password) and
+                  re.search(r"\d", password) and
+                  re.search(r"[!@#$%^&*()]", password)):
+            result = "Does not meet password complexity requirements"
+        elif not 6 <= len(password) <= 30:
+            result = "Password not of correct length"
+        elif not email.endswidth("@ucsd.edu"):
+            result = "Not a valid ucsd email"
+        else:
+            is_valid = True
+            result = password
+
+        return {"is_valid": is_valid, "result": result}
+
+    @staticmethod
+    def create_user_entry(email, full_name, user_hash):
+        is_success = False
+        try:
+            with closing(psycopg2.connect(CREDENTIALS)) as con:
+                with con, con.cursor() as cur:
+                    cur.excecute("""
+                                 insert into user_info(email, full_name, hash)
+                                     values(%s, %s, %s);""",
+                                 [email, full_name, user_hash])
+        except psycopg2.OperationalError:
+            result = falcon.HTTP_503, "Generic database error"
+        except psycopg2.errors.UniqueViolation:
+            result = falcon.HTTP_406, "User already exists"
+        else:
+            is_success = True
+            result = falcon.HTTP_201, "User entry created"
+
+        return {"is_success": is_success, "result": result}
+
+    def on_post(self, req, resp):
+        form = CreateUser.form_parser(req.media)
+        if not form.get("is_valid"):
+            resp.status = falcon.HTTP_406
+            resp.media = {"message": form.get("result")}
+            return
+
+        form = form.get("result")
+
+        form_validation = CreateUser.validate(form.get("email"),
+                                              form.get("password"),
+                                              form.get("confirm_password"))
+        if not form_validation.get("is_valid"):
+            resp.status = falcon.HTTP_406
+            resp.media = {"message": form_validation.get("result")}
+            return
+
+        def hasher(x): return argon2.PasswordHasher().hash(x)
+        user_hash = hasher(form_validation.get("result"))
+
+        entry_creation = CreateUser.create_user_entry(form.get("full_name"),
+                                                      form.get("email"),
+                                                      user_hash)
+
+        resp.status = entry_creation.get("result")[0]
+        resp.media = {"message": entry_creation.get("result")[1]}
+
+
+class GetFullName:
+    @staticmethod
+    def form_parser(form):
+        is_valid = True
+        try:
+            email = str.strip(form.get("email"))
+        except (KeyError, TypeError):
+            is_valid = False
+            result = "JSON Format Error"
+        else:
+            result = {"email": email}
+
+        return {"is_valid": is_valid, "result": result}
+
+    @staticmethod
+    def fetch_user_name(email):
+        is_success = False
+        try:
+            with closing(psycopg2.connect(CREDENTIALS)) as con:
+                with con, con.cursor() as cur:
+                    cur.execute("""
+                                select full_name from user_info
+                                    where email = %s;""",
+                                [email])
+                    name = cur.fetchone()[0]
+        except psycopg2.OperationalError:
+            result = falcon.HTTP_503, "Generic database error"
+        except TypeError: # None type return on no matches
+            result = falcon.HTTP_406, "User does not exist"
+        else:
+            is_success = True
+            result = falcon.HTTP_200, name
+
+        return {"is_success": is_success, "result": result}
+
+    def on_get(self, req, resp):
+        form = GetFullName.form_parser(req.media)
+        if not form.get("is_valid"):
+            resp.status = falcon.HTTP_406
+            resp.media = {"message": form.get("result")}
+            return
+
+        email = form.get("result").get("email")
+        fullname_retrieval = GetFullName.fetch_user_name(email)
+
+        resp.status = fullname_retrieval.get("result")[0]
+        if not fullname_retrieval.get("is_success"):
+            resp.media = {"message": fullname_retrieval.get("result")[1]}
+        else:
+            resp.media = {"name": fullname_retrieval.get("result")[1]}
+
+
+class UserLogin:
+    @staticmethod
+    def form_parser(form):
+        is_valid = True
+        try:
+            email = str.strip(form.get("email"))
+            password = str.strip(form.get("password"))
+        except (KeyError, TypeError):
+            is_valid = False
+            result = "JSON Form Error"
+        else:
+            result = {"email": email, "password": password}
+
+        return {"is_valid": is_valid, "result": result}
+
+    @staticmethod
+    def grab_user_hash(user):
+        is_success = False
+        try:
+            with closing(psycopg2.connect(CREDENTIALS)) as con:
+                with con, con.cursor() as cur:
+                    cur.execute("""
+                                select hash from user_info
+                                    where email=%s;""",
+                                [user])
+                    user_hash = cur.fetchone()[0]
+        except psycopg2.OperationalError:
+            result = falcon.HTTP_503, "Generic database error"
+        except TypeError: # None type return on no match
+            result = falcon.HTTP_406, "User does not exist"
+        else:
+            is_success = True
+            result = user_hash
+
+        return {"is_success": is_success, "result": result}
+
+    @staticmethod
+    def verify_identity(password, user_hash):
+        try:
+            argon2.PasswordHasher().verify(user_hash, password)
+        except argon2.exceptions.VerifyMismatchError:
+            is_valid = False
+            result = falcon.HTTP_401, "Incorrect password"
+        else:
+            is_valid = True
+            result = falcon.HTTP_200, "User logged in"
+
+        return {"is_valid": is_valid, "result": result}
+
+    def on_post(self, req, resp):
+        form = UserLogin.form_parser(req.media)
+        if not form.get("is_valid"):
+            resp.status = falcon.HTTP_406
+            resp.media = {"message": form.get("result")}
+            return
+
+        form = form.get("result")
+        user_hash = UserLogin.grab_user_hash(form.get("email"))
+
+        if not user_hash.get("is_success"):
+            resp.status = user_hash.get("result")[0]
+            resp.media = {"message": user_hash.get("result")[1]}
+            return
+
+        user_hash = user_hash.get("result")
+
+        verification = UserLogin.verify_identity(form.get("password"),
+                                                 user_hash)
+        resp.status = verification.get("result")[0]
+        resp.media = {"message": verification.get("result")[1]}
+        
 
 class ManageGroupEvents():
     def on_get(self, req, resp):
